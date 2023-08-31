27aab473b7358bc52701af0f6a940171d19f89d8
sajedul-dev <sajedulislamraja@gmail.com>
2023-08-31 19:40:00 +0600

    multiple section added

---
+++ js/script.js
@@ -0,0 +1,16 @@
+
+// ----------------- banner slick slider ----------------- 
+$('.bg_wrapper').slick({
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay:true,
+    autoplaySpeed:1000,
+    arrows:true,
+    dots:false,
+    prevArrow:'.left',
+    nextArrow:'.right',
+  });
+
+  // ----------------- categories slick slider ---------------- 
+
