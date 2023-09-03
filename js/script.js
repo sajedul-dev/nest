211c96f6318ec1bfaa3ecb7db7f9b01178ec40db
sajedul-dev <sajedulislamraja@gmail.com>
2023-09-04 03:04:09 +0600

    back to top and halka resposive done korlm

--- js/script.js
+++ js/script.js
@@ -1,6 +1,32 @@
 
-// ----------------- banner slick slider ----------------- 
-$('.bg_wrapper').slick({
+$(function(){
+
+  // -------------------- b to t ------------------------ 
+
+    $(".b-to-t") .click(function(){
+      $("body,html").animate({scrollTop:0},1500)
+    });
+
+    // ---------------- b to t show/hide ------------- 
+
+    $(window).scroll(function(){
+      let scroll = $(this).scrollTop()
+      if(scroll > 300){
+        $(".b-to-t").css(
+          "opacity" , "999"
+        )
+      }else{
+        $(".b-to-t").css(
+          "opacity" , "0"
+        )
+      }
+    })
+
+
+
+  // ----------------- banner slick slider ----------------- 
+
+  $('.bg_wrapper').slick({
     infinite: true,
     slidesToShow: 1,
     slidesToScroll: 1,
@@ -12,5 +38,5 @@ $('.bg_wrapper').slick({
     nextArrow:'.right',
   });
 
-  // ----------------- categories slick slider ---------------- 
+});
 
