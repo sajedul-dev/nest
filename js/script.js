f2421a21b52cb6050f352fbee571dcfd6a746a81
sajedul-dev <sajedulislamraja@gmail.com>
2023-09-09 13:56:40 +0600

    mixit-up menu added

--- js/script.js
+++ js/script.js
@@ -7,21 +7,56 @@ $(function(){
       $("body,html").animate({scrollTop:0},1500)
     });
 
-    // ---------------- b to t show/hide ------------- 
+    // ---------------- b to t show/hide and ------------- 
 
     $(window).scroll(function(){
       let scroll = $(this).scrollTop()
       if(scroll > 300){
         $(".b-to-t").css(
-          "opacity" , "999"
+          "opacity" , "1"
         )
       }else{
         $(".b-to-t").css(
           "opacity" , "0"
         )
       }
+
+      // ----------------- nav fixed operetion ---------------- 
+
+      if(scroll > 150){
+        $(".inner-nav").addClass("fixed")
+      }else{
+        $(".inner-nav").removeClass("fixed")
+      }
     })
 
+    // ---------------- preloader operation ------------------- 
+
+    $(window).on('load',function(){
+      $(".preloader-main").delay(2000).fadeOut(500)
+    })
+
+    // ----------- popular mixit up operation ------------ 
+
+    var mixer = mixitup(".product_wrapper", {
+      selectors: {
+          target: '.mix'
+      },
+      animation: {
+          duration: 300
+      }
+    });
+
+    // ----------- daily best sels mixit up operation ------------ 
+
+    var mixer = mixitup(".sels_wrapper", {
+      selectors: {
+          target: '.hello'
+      },
+      animation: {
+          duration: 400
+      }
+    });
 
 
   // ----------------- banner slick slider ----------------- 
@@ -37,6 +72,7 @@ $(function(){
     prevArrow:'.left',
     nextArrow:'.right',
   });
+
 
 });
 
